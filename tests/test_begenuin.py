34c8a17c4cc86ced1e8d21c3f0a20b17b9c637d4
Shi-kh <shikharathod462@gmail.com>
2025-06-21 19:22:49 +0530

    added method

--- tests/test_begenuin.py
+++ tests/test_begenuin.py
@@ -1,16 +1,16 @@
 import pytest
 from playwright.sync_api import Page, expect
 
-# @pytest.mark.parametrize("url", ["/home"])
-# def test_get_app_button_redirects(page: Page, url):
-#     page.goto("https://begenuin.com" + url)
-#     get_app_btn = page.locator('//button[contains(text(), "Get App")]')
-#     expect(get_app_btn).to_be_visible()
-#     get_app_btn.click()
-#     modal = page.locator('//p[@class="text-new-h4-mobile"]')
-#     expect(modal).to_be_visible()
-#     qr_code = page.locator('//canvas[@id="react-qrcode-logo"]') 
-#     expect(qr_code).to_be_visible()
+@pytest.mark.parametrize("url", ["/home"])
+def test_get_app_button_redirects(page: Page, url):
+    page.goto("https://begenuin.com" + url)
+    get_app_btn = page.locator('//button[contains(text(), "Get App")]')
+    expect(get_app_btn).to_be_visible()
+    get_app_btn.click()
+    modal = page.locator('//p[@class="text-new-h4-mobile"]')
+    expect(modal).to_be_visible()
+    qr_code = page.locator('//canvas[@id="react-qrcode-logo"]') 
+    expect(qr_code).to_be_visible()
 
 def test_login_invalid_credentials(page: Page):
     page.goto("https://begenuin.com")
@@ -26,19 +26,19 @@ def test_login_invalid_credentials(page: Page):
 
     password_input = page.locator("//input[@id='loginPassword']")
     expect(password_input).to_be_visible() 
-    
+
     submit_button = page.locator("//button[contains(text(),'Log In')]")
     expect(submit_button).to_be_visible()
 
-    # email_input.fill("test@example.com")
-    # password_input.fill("wrongpass")
-    # submit_button.click()
+    email_input.fill("test@example.com")
+    password_input.fill("wrongpass")
+    submit_button.click()
 
-    # Optionally validate error message
-    # error_msg = page.locator("//div[@class='rnc__notification-message']")  
-    # expect(error_msg).to_be_visible()
+    Optionally validate error message
+    error_msg = page.locator("//div[@class='rnc__notification-message']")  
+    expect(error_msg).to_be_visible()
 
-# def test_become_creator_badge(page: Page):
-#     page.goto("https://begenuin.com/home")
-#     badge = page.locator("//p[@class='w-64 overflow-hidden p-3 text-start text-body-1-bold']")
-#     expect(badge).to_be_visible()
+def test_become_creator_badge(page: Page):
+    page.goto("https://begenuin.com/home")
+    badge = page.locator("//p[@class='w-64 overflow-hidden p-3 text-start text-body-1-bold']")
+    expect(badge).to_be_visible()
